bcdd3c83fe3cd5a299b400bb8c11144e1d323d86
TatoSk8 <cristianballesteros1@gmail.com>
2021-01-20 19:40:59 -0500

    Destructing Objects

--- Objects.js
+++ Objects.js
@@ -9,17 +9,37 @@ var Dario = {
     age: 23
 }
 
-// function printNameinCapitalLetters(person) {  
-//     console.log(person.name.toUpperCase())
-// }
+function printNameinCapitalLetters(person) {  
+    console.log(person.name.toUpperCase())
+}
 
 function printNameinCapitalLetters({name}) {  
     console.log(name.toUpperCase())
 }
 
-printNameinCapitalLetters (Cristian); 
-printNameinCapitalLetters (Dario); 
-printNameinCapitalLetters ({name: "Pepito"})
+ printNameinCapitalLetters (Cristian); 
+ printNameinCapitalLetters (Dario); 
+ printNameinCapitalLetters ({name: "Pepito"})
 //printNameinCapitalLetters(); ERROR , We cannot destructure an object with out send the object that he waits, the atribute name
 
 //printNameinCapitalLetters ({ Surname: "Gomez"}) this is also an error because the atribute of he function isn't Surname, it's name! 
+
+//DESTRUCTING OF OBJECTS 
+function printNameinCapitalLetters(person) {  
+    //var name = person.name
+    var {name} = person
+
+    console.log(name.toUpperCase())
+}
+printNameinCapitalLetters (Cristian); 
+printNameinCapitalLetters (Dario); 
+
+//Challengue Print Name n Age 
+
+function printNameNAge (person){
+    var {name, age} = person
+    console.log(`Hi my name is ${name} and im ${age}`);
+}
+printNameNAge(Dario);
+printNameNAge(Cristian);
+
