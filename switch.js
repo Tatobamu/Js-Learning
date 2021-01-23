9381cf88c080f9b6b9ea132ec8947a8c67a82b29
TatoSk8 <cristianballesteros1@gmail.com>
2021-01-23 16:14:46 -0500

    Switch class with first aplication of prompt function

---
+++ switch.js
@@ -0,0 +1,45 @@
+//prompt function ask for data to the user
+
+var sign = prompt("What is your zoodiac sign") 
+
+
+switch (sign) {
+    case 'Aries':
+        console.log('You are just like James Bond except your license is to thrill.')
+        break
+    case 'Tauro':
+        console.log('Isnt it exhausting to try and be the person in charge all the time? Sit back and let the universe.')
+        break
+    case 'Geminis':
+        console.log('Jumping to conclusions is a fun and easy sport, but they dont give medals for it at the Olympics')
+        break
+    case 'Cancer':
+        console.log('If you cant find anything, it might be time for some heavy-duty closet cleaning.')
+        break
+    case 'Leo':
+        console.log('Why not let someone else lead for a change? You have gotten so used to being in charge that you have...')
+        break
+    case 'Virgo':
+        console.log('Money changes everything, especially if you are having problems with it. In and of itself, money is')
+        break
+    case 'Libra':
+        console.log('Someone (and you know exactly who it is) used to do a lot of nice things for you, but what have...')
+        break
+    case 'Escorpio':
+        console.log('Think of yourself as Switzerland, especially if two people you care about are involved in a...')
+        break
+    case 'Sagitario':
+        console.log('How long has this been going on? Could it be that while you werent looking something kindled')
+        break
+    case 'Capricornio':
+        console.log('What the heck was that? Did you just feel the earth move under your feet? Lately it feels like...')
+        break
+    case 'Acuario':
+        console.log('Your brain is like a bottle of champagne right now: all bubbles and fizz and tons of excitement...')
+        break
+    case 'Piscis':
+        console.log('Somethings obscuring your vision like a veil. If you concentrate, you might be able to lift a..')
+        break
+    default:
+        console.log('Isnt a zodiac sign, If your birthday its today you are a person: extravagant in your affective relationships but passionate in the intimity')
+}
